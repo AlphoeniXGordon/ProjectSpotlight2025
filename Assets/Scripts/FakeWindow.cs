b41e6cd02bbd23de0425c368a2cc201e85258d22
RenderDog <513069073@qq.com>
2025-10-16 23:06:18 +0800

    fake window minimized

--- Assets/Scripts/FakeWindow.cs
+++ Assets/Scripts/FakeWindow.cs
@@ -1,15 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class FakeWindow : MonoBehaviour
 {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    private const int SW_SHOWMINIMIZED = 2;
+
     public void QuitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
+#endif
+    }
+
+    public void MinimizedGame()
+    {
+#if !UNITY_Editor
+        ShowWindow(GetActiveWindow(), SW_SHOWMINIMIZED);
 #endif
     }
 }
