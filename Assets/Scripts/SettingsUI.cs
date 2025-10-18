e60f3ee0e7c9d703392685b8dec6a77daf20cded
RenderDog <513069073@qq.com>
2025-10-18 19:03:07 +0800

    add settings

---
+++ Assets/Scripts/SettingsUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsUI : MonoBehaviour
+{
+    public void SwitchLanguageChinese()
+    {
+        
+    }
+
+    public void SwitchLanguageMongulia()
+    {
+
+    }
+
+    public void MuteVoice()
+    {
+
+    }
+
+    public void QualityEpic()
+    {
+
+    }
+
+    public void QualityLow()
+    {
+
+    }
+
+    public void SetVoiceLevel(float newLevel)
+    {
+
+    }
+}
